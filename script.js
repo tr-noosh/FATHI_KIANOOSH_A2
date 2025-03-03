5d36fb05cf5cef9673a401565d06e12ec6a9ba71
tr-noosh <kianooshfathi@cmail.carleton.ca>
2025-03-03 10:16:34 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var bigpic = document.getElementById('bigpic');
+
+function nav(page) {   
+	window.location.href=page;
+}
+
+function gallery(number) {
+	bigpic.src = `/images/photos/${parseInt(number)}.png`;
+}
+
+function pick_location(name) {
+	[...document.getElementsByClassName('location')].forEach(loc=>{ loc.style.display = 'none'; })
+	document.getElementById(name).style.display = 'block';
+}
+
+[...document.getElementsByTagName('img')].forEach(img => {
+	if (img.hasAttribute('alt') && !img.hasAttribute('title')) {
+		img.title = img.alt;
+	}
+});
